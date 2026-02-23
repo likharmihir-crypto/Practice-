18906e6f14e73c4d844320c85cf0afa62e3d7bf8
Mihir Likhar <likharmihir@gmail.com>
2026-02-23 23:06:08 +0530

    Updated file

--- src/Default/Demo.java
+++
@@ -1,9 +0,0 @@
-package Default;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Scanner;
-class Practice{
-public static void main(String a[]) {
-System.out.print("Hare Krishna");
-}
-}
---
+++ src/Default/Prac.java
@@ -0,0 +1,7 @@
+package Default;
+
+public class Prac {
+	public static void main(String a[]) {
+     System.out.println("hare Krishna");
+}
+}
