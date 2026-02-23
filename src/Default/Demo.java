6fa7aaf255aa018d75bd97477b718939eb41dc13
Mihir Likhar <likharmihir@gmail.com>
2026-02-23 22:50:33 +0530

    initial commit

---
+++ src/Default/Demo.java
@@ -0,0 +1,9 @@
+package Default;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
+class Practice{
+public static void main(String a[]) {
+System.out.print("Hare Krishna");
+}
+}
